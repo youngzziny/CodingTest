c9632c6941dbec27e8db0003bfddee3bbb923ec4
Zoey Young <84887974+youngzziny@users.noreply.github.com>
2024-07-26 17:59:41 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = n/slice ;
+        
+        if(n%slice != 0){
+            answer++;
+        }
+        
+        return answer;
+    }
+}
